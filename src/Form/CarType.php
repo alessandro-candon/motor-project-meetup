81f95ef98cc2fc8a5b1f6acaafaba426f7e65053
Alessandro Candon <alessandro.candon@decathlon.com>
2020-02-08 09:02:53 +0100

    create form and added all types at the files

---
+++ src/Form/CarType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Car;
+use Doctrine\DBAL\Types\DateType;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CarType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('brand', ChoiceType::class, [
+                'choices'  => [
+                    'fiat' => 'Fiat',
+                    'renault' => 'Renault',
+                    'alfa' => 'Alfa',
+                    'bmw' => 'BMW',
+                ]
+            ])
+            ->add('createdAt', DateType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Car::class,
+        ]);
+    }
+}
