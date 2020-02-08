da311fcf697a2c78b49037a9e3f2e22df34831ba
Alessandro Candon <alessandro.candon@decathlon.com>
2020-02-08 09:11:45 +0100

    from bad formatted

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use App\Entity\Car;
+use App\Form\CarType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CarController extends AbstractController
@@ -14,8 +15,14 @@ class CarController extends AbstractController
      */
     public function index()
     {
+        /** @var Car $car */
+        $car = new Car();
+        /** @var FormInterface $carForm */
+        $carForm = $this->createForm(CarType::class, $car);
+
         return $this->render('car/index.html.twig', [
             'controller_name' => 'CarController',
+            'car_form' => $carForm->createView()
         ]);
     }
 }
--- src/Form/CarType.php
+++ src/Form/CarType.php
@@ -3,10 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Car;
-use Doctrine\DBAL\Types\DateType;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
