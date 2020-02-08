894c27b9f8189b9fc49a48334c5480cd3ff1e6b3
Alessandro Candon <alessandro.candon@decathlon.com>
2020-02-08 08:25:05 +0100

    401 -> response

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CarController extends AbstractController
 {
     /**
+     * @IsGranted("ROLE_ADMIN")
+     * @Security("is_granted('ROLE_ADMIN') and is_granted('ROLE_FRIENDLY_USER')")
      * @Route("/car", name="car")
      */
     public function index()
