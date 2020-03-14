84313a173d7a2e77508637cc875152628d4109ca
Alessandro Candon <alessandro.candon@decathlon.com>
2020-03-14 18:45:55 +0100

    add easy adm with entity list on params

--- src/Form/CarType.php
+++ src/Form/CarType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Car;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -12,17 +13,20 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CarType extends AbstractType
 {
+    /** @var array array $brandTypes */
+    private $brandTypes = [];
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->brandTypes = $container->getParameter('brand_types');
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name', TextType::class)
             ->add('brand', ChoiceType::class, [
-                'choices'  => [
-                    'fiat' => 'Fiat',
-                    'renault' => 'Renault',
-                    'alfa' => 'Alfa',
-                    'bmw' => 'BMW',
-                ]
+                'choices'  => $this->brandTypes
             ])
             ->add('createdAt', DateType::class)
         ;
